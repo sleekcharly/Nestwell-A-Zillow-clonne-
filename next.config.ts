5b987f736d984d9e16f0862f7505400a3009f03f
sleekcharly <c_ukasoanya@yahoo.com>
2026-02-06 13:05:48 +0100

    added clerk/skills

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["http://localhost:3000", "http://192.168.0.111:3000"],
 };
 
 export default nextConfig;
